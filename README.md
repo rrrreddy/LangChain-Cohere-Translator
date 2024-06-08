4bbe235e57acc2b5fcadee8ecc239f321ff5a0c9
Raghu <rrrreddy07@gmail.com>
2024-06-07 21:16:04 -0500

    LangChain chains as a REST API serve.py file created

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LangChain-Cohere-Translator
+
+Langchain Translator application with Cohere LLM command-r model with LCEL (LangChain Expression Language)
+
---
+++ serve.py
@@ -0,0 +1,65 @@
+from typing import List
+
+#import for prompts
+from langchain_core.prompts import ChatPromptTemplate
+#import for model 
+from langchain_cohere import ChatCohere
+# import for output parsing
+from langchain_core.output_parsers import StrOutputParser
+import os
+import getpass
+from langserve import add_routes
+
+from fastapi import FastAPI
+
+os.environ["COHERE_API_KEY"] = getpass.getpass("Enter your Cohere API Key:")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+
+# 1. Creating Prompt Template
+
+system_template = "Translate the following message/question asked from english into {language} in english language:"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template),
+        ("user", "{text}")
+    ]
+)
+
+
+# 2. invoking model
+
+model = ChatCohere(model="command-r")
+
+# 3. Output Parsing
+
+parser  = StrOutputParser()
+
+# building chain - runnable sequence
+
+chain  = prompt_template | model | parser
+
+
+
+# App defination
+
+app = FastAPI(
+    title= "langchain-cohere server",
+    version= "0.1",
+    description= "A simple langchain-cohere API server using LangChain's runnable interface"
+)
+
+# Adding chain route
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
+
